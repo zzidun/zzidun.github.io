2f94a1ac292646d0c1b2f9f2eac404013bd84110
zzidun <zzidun@outlook.com>
2024-03-20 00:45:19 +0800

    tag page

--- site.hs
+++ site.hs
@@ -27,11 +27,26 @@ main = do
                 >>= loadAndApplyTemplate "templates/default.html" defaultContext
                 >>= relativizeUrls
 
+        tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+        tagsRules tags $ \tag pattern -> do
+            let title = "Posts tagged \"" ++ tag ++ "\""
+            route idRoute
+            compile $ do
+                posts <- recentFirst =<< loadAll pattern
+                let ctx = constField "title" title
+                        `mappend` listField "posts" (postCtxWithTags tags) (return posts)
+                        `mappend` defaultContext
+
+                makeItem ""
+                    >>= loadAndApplyTemplate "templates/tag.html" ctx
+                    >>= loadAndApplyTemplate "templates/default.html" ctx
+                    >>= relativizeUrls
+
         match "posts/*" $ do
             route $ setExtension "html"
             compile $ pandocCompiler
-                >>= loadAndApplyTemplate "templates/post.html"    postCtx
-                >>= loadAndApplyTemplate "templates/default.html" postCtx
+                >>= loadAndApplyTemplate "templates/post.html"    (postCtxWithTags tags)
+                >>= loadAndApplyTemplate "templates/default.html" (postCtxWithTags tags)
                 >>= relativizeUrls
 
         create ["archive.html"] $ do
@@ -65,6 +80,10 @@ main = do
 
 
 --------------------------------------------------------------------------------
+
+postCtxWithTags :: Tags -> Context String
+postCtxWithTags tags = tagsField "tags" tags `mappend` postCtx
+
 postCtx :: Context String
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
